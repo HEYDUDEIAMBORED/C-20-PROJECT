5447506da7148db29a4eec5a06d5f99bf8d1ec55
HEYDUDEIAMBORED <73388007+HEYDUDEIAMBORED@users.noreply.github.com>
2020-11-10 16:02:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,8 +30,7 @@ function draw() {
   }
   drawSprites();
 }
-function collision(object1,obeject2){
- 
+function collision(object1,obeject2){ 
   if ( object1.y-obeject2.y<obeject2.height/2+object1.height/2 &&
     obeject2.y-object1.y<obeject2.height/2+object1.height/2 && 
      obeject2.x-object1.x<obeject2.width/2+object1.width/2 &&
